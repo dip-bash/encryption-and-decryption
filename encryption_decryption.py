99b35e2ec8cc327a41a282be25c94055e0ed6c30
dip-bash <saumyadip.social@gmail.com>
2024-05-20 11:29:24 +0530

    v 0.1.0 --fix key

--- encryption_decryption.py
+++ encryption_decryption.py
@@ -1,14 +1,40 @@
 def Encode():
-    key = int(input("input kay: "))
+    key=input("enter key: ")
+    char=key
+    try:
+        key=int(key)
+    except:
+        print("invalid input")
+        exit()
+
+# rebuild key:
+    if (key>=131 or key<=0 or key==109 or 63<=key<=93):
+        add=0
+        char=list(char)
+        for i in range(len(char)):
+            temp=int(char[i])
+            add=add+temp
+        if(add>130):
+            key=123
+        elif(add==109):
+            key=110
+        elif(63<=add<=93):
+            key=61
+        else:
+            key=add
+
+# main encoding
     msg = input("type your massage: ")
     msg = list(msg)
     temp = 0
+    print("The encrypted msg is => " ,end="")
     for letters in msg:
         if letters == " ":
             letters = "@"
 
         temp = ord(letters) + key
         print(chr(temp), end="")
+        # adding noise
         temp = hex(temp)
         print(temp[2:], end="")
 
@@ -16,10 +42,35 @@ def Encode():
 
 
 def Decode():
-    key = int(input("input key: "))
+    key=input("enter key: ")
+    char=key
+    try:
+        key=int(key)
+    except:
+        print("invalid input")
+        exit()
+
+# rebuild key:
+    if (key>=131 or key<=0 or key==109 or 63<=key<=93):
+        add=0
+        char=list(char)
+        for i in range(len(char)):
+            temp=int(char[i])
+            add=add+temp
+        if(add>130):
+            key=123
+        elif(add==109):
+            key=110
+        elif(63<=add<=93):
+            key=61
+        else:
+            key=add
+
+# main decoding
     x = input("input massage: ")
     y = len(x)
     msg = []
+    # removing noise
     for i in range(0, y, 3):
         msg.append(x[i])
     temp = 0
@@ -31,7 +82,7 @@ def Decode():
             print(chr(temp), end="")
     print("\n")
 
-
+# main body
 x = input("enter 1 for encode\nenter 2 for decode\n==> ")
 match x:
 
